8c3bb42541bd8623fe94c78c22fcd5aa6ac283f4
Luca Park <qlife1146@gmail.com>
2024-08-25 16:34:15 +0900

    겨우 firebase 안정화...

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:vscode/firebase_options.dart';
 
 import 'dart/screen/is_local_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 

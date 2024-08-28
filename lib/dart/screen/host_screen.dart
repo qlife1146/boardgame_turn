671fc3f544427d4fc86200be4057c760cf36f710
Luca Park <qlife1146@gmail.com>
2024-08-29 01:39:43 +0900

    1. back 하면 생성된 컬렉션 삭제 2. Host 이름 빈 칸이면 기본값 설정

--- lib/dart/screen/host_screen.dart
+++ lib/dart/screen/host_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:ffi';
 import 'dart:isolate';
+import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -11,86 +13,120 @@ class HostScreen extends StatefulWidget {
   State<HostScreen> createState() => _HostScreenState();
 }
 
-Future<void> updateRoomActivity(String roomCode) async {
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final roomDoc = _firestore.collection('rooms').doc(roomCode);
+// Future<void> updateRoomActivity(String roomCode) async {
+//   FirebaseFirestore _firestore = FirebaseFirestore.instance;
+//   final roomDoc = _firestore.collection('rooms').doc(roomCode);
 
-  await roomDoc.update({
-    'lastActive': FieldValue.serverTimestamp(),
-  });
-}
+//   await roomDoc.update({
+//     'lastActive': FieldValue.serverTimestamp(),
+//   });
+// }
 
 class _HostScreenState extends State<HostScreen> {
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _nameController = TextEditingController();
-  String _roomCode = "111111";
+  String _roomCode = "******";
   bool isRoomOpen = false;
   List<String> guests = [];
 
   @override
   void initState() {
     super.initState();
-    _generateRoomCode();
+    // _generateRoomCode();
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _nameController,
-            decoration: InputDecoration(labelText: "Your name"),
-          ),
-          Text("Room Code: $_roomCode"),
-          ElevatedButton(
-            onPressed: isRoomOpen ? _closeRoom : _openRoom,
-            child: Text(isRoomOpen ? "Closed" : "Opened"),
-          ),
-          Expanded(
-            child: StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('rooms')
-                  .doc(_roomCode)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
+    return PopScope(
+      onPopInvokedWithResult: (didPop, result) async {
+        try {
+          await FirebaseFirestore.instance
+              .collection('rooms')
+              .doc(_roomCode)
+              .delete();
+        } catch (e) {
+          debugPrint("Error deleting room: $e");
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: "Your name"),
+              ),
+              ElevatedButton(
+                onPressed: isRoomOpen ? null : _createRoom,
+                child: Text("Create Room"),
+              ),
+              Text("Room Code: $_roomCode"),
+              ElevatedButton(
+                onPressed: isRoomOpen ? _closeRoom : null,
+                child: Text(isRoomOpen ? "Closed" : "Opened"),
+              ),
+              Expanded(
+                child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('rooms')
+                      .doc(_roomCode)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
 
-                var roomData = snapshot.data!.data() as Map<String, dynamic>;
-                guests = List<String>.from(roomData['guests'] ?? []);
+                    var roomData = snapshot.data!.data()
+                        as Map<String, dynamic>?; // null 체크
+                    if (roomData == null) {
+                      return Center(child: Text('No room data available'));
+                    }
 
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(guests[index]),
-                      trailing: IconButton(
-                        onPressed: () {
-                          _removeGuest(guests[index]);
-                        },
-                        icon: Icon(Icons.remove_circle),
-                      ),
+                    guests = List<String>.from(roomData['guests'] ?? []);
+
+                    return ListView.builder(
+                      itemCount: guests.length,
+                      itemBuilder: (context, index) {
+                        if (index < guests.length) {
+                          return ListTile(
+                            title: Text(guests[index]),
+                            trailing: IconButton(
+                              onPressed: () {
+                                _removeGuest(guests[index]);
+                              },
+                              icon: Icon(Icons.remove_circle),
+                            ),
+                          );
+                        } else {
+                          return SizedBox();
+                        }
+                      },
                     );
                   },
-                );
-              },
-            ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 
-  void _generateRoomCode() async {
+  Future<void> _generateRoomCode() async {
     final roomCollection = _firestore.collection('rooms');
     String generatedRoomCode;
 
     do {
-      generatedRoomCode = (100000 +
-              (999999 - 100000) *
-                  (new DateTime.now().millisecondsSinceEpoch % 1000000))
-          .toString();
+      final min = 100000;
+      final max = 999999;
+
+      // min과 max 사이의 랜덤 숫자 생성
+      generatedRoomCode = (min + Random().nextInt(max - min + 1)).toString();
+      // generatedRoomCode = (100000 +
+      //         (999999 - 100000) *
+      //             (new DateTime.now().millisecondsSinceEpoch % 100000))
+      // .toString();
+
       final snapshot = await roomCollection.doc(generatedRoomCode).get();
       if (!snapshot.exists) {
         _roomCode = generatedRoomCode;
@@ -133,5 +169,25 @@ class _HostScreenState extends State<HostScreen> {
       'guests': guests,
     });
     setState(() {});
+  }
+
+  void _createRoom() async {
+    await _generateRoomCode();
+    if (_nameController.text == "") {
+      _nameController.text = "HOST";
+    }
+    // 방 생성 및 Firestore에 저장
+    final roomCollection = _firestore.collection('rooms');
+    await roomCollection.doc(_roomCode).set(
+      {
+        'host': _nameController.text,
+        'guests': [],
+        'isOpen': true,
+      },
+    );
+
+    setState(() {
+      isRoomOpen = true;
+    });
   }
 }
